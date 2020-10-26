cfb3c0a8f7dc425ca58e40ca906a0ea3440360ca
s-satsangi <s.satsangi@gmail.com>
2020-10-25 23:28:15 -0400

    I think your spec doesn't work.

---
+++ app/application.rb
@@ -0,0 +1,20 @@
+class Application
+
+    def call(env)
+        resp = Rack::Response.new
+
+        time = Time.new
+
+        #time.hour < 12 ? resp.write("Good Morning!") : resp.write("Good Afternoon!")
+        
+        if time.hour < 12 
+             resp.write("Good Morning!") 
+        else
+             resp.write("Good Afternoon!")
+        end
+
+        resp.finish
+    end
+
+
+end
